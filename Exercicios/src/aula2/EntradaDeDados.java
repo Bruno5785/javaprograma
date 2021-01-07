169c141aa3e17275b8557b838e7d30d9c4e4854f
brunp <brunp@192.168.10.131>
2021-01-07 20:24:21 -0300

    commit inicial do projeto Exercicios.

---
+++ Exercicios/src/aula2/EntradaDeDados.java
@@ -0,0 +1,23 @@
+package aula2;
+
+import java.util.Scanner;
+
+public class EntradaDeDados {
+
+	public static void main(String[] args) {
+		lendoNumeros();
+
+	}
+
+	public static void lendoNumeros(){
+		int n1;    // números inteiros 
+		double n2; // valores decimais 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite um número inteiro:" );
+		n1 = sc.nextInt();  //lê um inteiro 
+		System.out.print("Digite um nunero real");
+		n2 = sc.nextDouble();
+		System.out.println("inteiro = " + n1 + "\nDecimal = "+ n2);
+		sc.close();
+	}
+}
